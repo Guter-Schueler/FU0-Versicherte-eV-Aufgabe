1862fef0c35288748b7c974064b5cc946ea80026
Guter-Schueler <111498614+Guter-Schueler@users.noreply.github.com>
2023-03-31 10:53:31 +0200

    force single page behaviour cause of stackblitz

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const loginPage = document.getElementById('login-page');
+const mainPage = document.getElementById('main-page');
+
+// LOGIN PAGE
+
 const form = document.querySelector('form');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -6,9 +11,67 @@ form.addEventListener('submit', function (event) {
   const password = form.elements['password'].value;
 
   if (username === 'mockUser' && password === 'pw') {
-    alert('Login erfolgreich!');
-    // Hier können Sie Ihre gewünschte Aktion ausführen, z.B. zur Startseite weiterleiten
+    loginPage.classList.add('hidden');
+    loginPage.classList.remove('hidden');
   } else {
     alert('Benutzername oder Passwort ungültig!');
+  }
+});
+
+// MAIN PAGE
+
+const API_URL = 'https://yg3v4d.deta.dev/subscribers';
+const contactForm = document.getElementById('contactForm');
+const fetchSubsBtn = document.getElementById('fetchSubs');
+
+async function fetchSubscribers() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchSubsBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetchSubscribers();
+});
+
+contactForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const first_name = document.getElementById('firstName').value;
+  const last_name = document.getElementById('lastName').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const city = document.getElementById('city').value;
+  const street_adress = document.getElementById('streetName').value;
+  const postal_code = document.getElementById('postalCode').value;
+  const noticed = document.getElementById('comment').value;
+  const agreeToPrivacyPolicy = document.getElementById(
+    'agreeToPrivacyPolicy'
+  ).checked;
+
+  if (!agreeToPrivacyPolicy) {
+    alert('Please agree to out privacy policy.');
+    return;
+  }
+
+  const formData = {
+    first_name,
+    last_name,
+    birthdate,
+    street_adress,
+    postal_code,
+    city,
+    noticed,
+  };
+
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+  } catch (error) {
+    console.error(error);
   }
 });
